b62aaa5952c84b661cce72594c37b7a721fa40b8
Samuel Rouse <samuel.rouse@gmail.com>
2018-03-10 15:18:49 -0500

    Add JSDoc to translate

--- src/translate.mjs
+++ src/translate.mjs
@@ -1,9 +1,10 @@
 /**
  * Shift all points on the specified axis so the lowest is at zero
  * @param {Array} points - An array of points to manipulate
- * @param {number} axisIndex - 0 = x, 1 = y, 2 = z
+ * @param {number} [axisIndex] - 0 = x, 1 = y, 2 = z
+ * @returns {Array} Updated Points
  */
-const moveBottomToAxis = (points, axisIndex) => {
+const moveBottomToAxis = (points, axisIndex = 0) => {
   const axisPoints = points.map(point => point[axisIndex]);
   const base = Math.min(...axisPoints);
 
