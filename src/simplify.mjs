2aa9aedd5c4da40531de0666fad6b54995ab55a9
Samuel Rouse <samuel.rouse@gmail.com>
2018-03-10 13:52:47 -0500

    Add JSDoc

--- src/simplify.mjs
+++ src/simplify.mjs
@@ -24,10 +24,24 @@ const getDuplicatePoints = points => points.reduce((acc, val, idx, arr) => {
   return acc;
 }, {});
 
+/**
+ * Provide the percentage, as a decimal, of duplicate points in the object
+ * @param {Array} points
+ * @param {Array} [dupPoints]
+ * @returns {number}
+ */
 const getDuplicatePercentage = (points, dupPoints = getDuplicatePoints(points)) =>
   Object.keys(dupPoints).length / points.length;
 
-
+/**
+ * Accept a face, set of original points, and a set of new (reindexed) points
+ * Re-index the face array against newPoints, adding to to it (immutably) as needed
+ * Return a version of the face with updated indices and the points used.
+ *
+ * @param {Array} face - the array of indices in origPoints
+ * @param {Array} origPoints - set of original points
+ * @param {Array} newPoints - the set of new points
+ */
 const cleanFace = (face, origPoints, newPoints) => face.reduce((acc, pointIdx) => {
   const origPoint = origPoints[pointIdx];
   let matchPoint = findFirstIndexOfPoint(origPoint.toString(), acc.points);
@@ -47,7 +61,9 @@ const cleanFace = (face, origPoints, newPoints) => face.reduce((acc, pointIdx) =
 });
 
 /**
- *
+ * There may be a better way to clean the faces, but this is the first attempt
+ * Walk through every face, checking every point each time.
+ * We can use this to compare any new methods
  * @param {Array} points
  * @param {Array} faces
  */
