f2e3bfb5597a82e4f685540df9b9a0461bf3cf58
Samuel Rouse <samuel.rouse@gmail.com>
2018-03-10 16:02:41 -0500

    Abstract Translate
    Provide translate abstractions for reuse.

--- src/translate.mjs
+++ src/translate.mjs
@@ -1,21 +1,42 @@
 /**
+ * Adjust the values of [x, y, z] points on one axis by an amount
+ * @param {number} axisIndex
+ * @param {number} amount
+ * @param {Array} points
+ * @returns {Array} Modified Points
+ */
+const shiftOnAxis = (axisIndex, amount, points) =>
+  points.map((point) => {
+    const newPoint = [...point];
+    newPoint[axisIndex] += amount;
+    return newPoint;
+  });
+
+/**
+ * Shift all points on the specified axis so the lowest is at zero
+ * @param {Array} points - An array of points to manipulate
+ * @param {number} [axisIndex] - 0 = x, 1 = y, 2 = z
+ * @param {boolean} [moveTop] - whether to shift the top or bottom
+ * @returns {Array} Updated Points
+ */
+const moveEdgeToAxis = (points, axisIndex = 0, moveTop = false) => {
+  const axisPoints = points.map(point => point[axisIndex]);
+  const base = Math[moveTop ? 'max' : 'min'](...axisPoints);
+
+  return shiftOnAxis(axisIndex, -base, points);
+};
+
+
+/**
  * Shift all points on the specified axis so the lowest is at zero
  * @param {Array} points - An array of points to manipulate
  * @param {number} [axisIndex] - 0 = x, 1 = y, 2 = z
  * @returns {Array} Updated Points
  */
-const moveBottomToAxis = (points, axisIndex = 0) => {
-  const axisPoints = points.map(point => point[axisIndex]);
-  const base = Math.min(...axisPoints);
-
-  return points.map((point) => {
-    const newPoint = [...point];
-    newPoint[axisIndex] -= base;
-    return newPoint;
-  });
-};
-
+const moveBottomToAxis = (points, axisIndex = 0) => moveEdgeToAxis(points, axisIndex, false);
 
 export default {
   moveBottomToAxis,
+  moveEdgeToAxis,
+  shiftOnAxis,
 };
