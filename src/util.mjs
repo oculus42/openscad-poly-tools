ddc44a68426af0a4e63863658e95c013e1ed6b5c
Samuel Rouse <samuel.rouse@gmail.com>
2018-03-14 08:03:25 -0400

    Add utilities

--- src/util.mjs
+++ src/util.mjs
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 
 /**
  * Locate the first index of a point in the points array
@@ -8,6 +9,32 @@
 const findFirstIndexOfPoint = (pointString, points) =>
   _.findIndex(points, point => point.toString() === pointString);
 
+/**
+ * Extract the used point indices from the existing faces
+ * @param faces
+ * @returns {Array}
+ */
+const getUniqueIndexes = faces => _.chain(faces).flatten(true).uniq().value();
+
+/**
+ * Map of indices from one array of points to the other
+ * @param {Array} origPoints
+ * @param {Array} uniquePoints - the set of points without duplicates
+ * @returns {object}
+ */
+const mapUniquePoints = (origPoints, uniquePoints) => origPoints.reduce((acc, val, idx) => {
+  const firstIdx = findFirstIndexOfPoint(val.toString(), uniquePoints);
+
+  // Have to handle both duplicates and the early matches
+  if (idx !== firstIdx || acc[idx] === undefined) {
+    acc[idx] = firstIdx;
+  }
+
+  return acc;
+}, {});
+
 export default {
   findFirstIndexOfPoint,
+  getUniqueIndexes,
+  mapUniquePoints,
 };
