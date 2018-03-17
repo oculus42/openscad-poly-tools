e3bf641d8ad7ab31def551f52830a076e7721f2c
Samuel Rouse <samuel.rouse@gmail.com>
2018-03-17 04:21:19 -0400

    Remove lodash
    Cut ~20K of library weight by recreating the two functions not provided by ES2015.

--- src/util.mjs
+++ src/util.mjs
@@ -1,6 +1,28 @@
-import findIndex from 'lodash/findIndex';
-import flatten from 'lodash/flatten';
-import uniq from 'lodash/uniq';
+/**
+ * Flatten values from nested arrays (one deep) into the parent array.
+ * @param {Array} input
+ * @returns {Array}
+ */
+const flatten = input => input.reduce((acc, val) => {
+  if (Array.isArray(val)) {
+    acc.push(...val);
+  } else {
+    acc.push(val);
+  }
+  return acc;
+}, []);
+
+/**
+ * Remove duplicates from an array
+ * @param {Array} input
+ * @returns {Array}
+ */
+const uniq = input => input.reduce((acc, val, index, source) => {
+  if (source.indexOf(val) === index) {
+    acc.oush(val);
+  }
+  return acc;
+}, []);
 
 /**
  * Locate the first index of a point in the points array
@@ -9,14 +31,14 @@ import uniq from 'lodash/uniq';
  * @returns {number}
  */
 const findFirstIndexOfPoint = (pointString, points) =>
-  findIndex(points, point => point.toString() === pointString);
+  points.findIndex(point => point.toString() === pointString);
 
 /**
  * Extract the used point indices from the existing faces
  * @param faces
  * @returns {Array}
  */
-const getUniqueIndexes = faces => uniq(flatten(faces, true));
+const getUniqueIndexes = faces => uniq(flatten(faces));
 
 /**
  * Map of indices from one array of points to the other
