fe7aff2cfc39b33d470a2d1901dac86bcc3116a5
Samuel Rouse <samuel.rouse@gmail.com>
2018-03-18 16:42:48 -0400

    Add export for load
    For some reason import.mjs doesn't want to behave the same as the other files, so working around that for now.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { filterForMatch, removeDeadPoints } from './edit.mjs';
 import { reportDuplicatePercentage, simplify } from './simplify.mjs';
 import { center, moveToOrigin, translate } from './translate.mjs';
+import importer from './import.mjs';
+
+const { load } = importer;
 
 export {
   center,
   filterForMatch,
+  load,
   moveToOrigin,
   removeDeadPoints,
   reportDuplicatePercentage,
